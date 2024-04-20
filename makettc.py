c57a48ce0a7ef6c8ab63adde8a6d14bdf142c7f8
MihailJP <mihailjp@gmail.com>
2024-04-20 16:51:58 +0900

    Fix makettc.py

--- makettc.py
+++ makettc.py
@@ -2,4 +2,4 @@
 from sys import argv
 font = fontforge.open(argv[2])
 fonts = [fontforge.open(filename) for filename in argv[3:len(argv)]]
-font.generateTtc(argv[1], fonts)
+font.generateTtc(argv[1], fonts, layer=1)
